af3ce84eb4575843588193adbb6faa8c3ac571f5
Kevin McAleer <kevinmcaleer@me.com>
2019-01-04 19:18:09 +0000

    Initial Commit

---
+++ Camera.py
@@ -0,0 +1,34 @@
+# Grab a video feed from the camera and pass to the classifer
+# TwitcherPi - AI Bird Counter
+# By Kevin McAleer
+# January 2019
+
+from picamera import PiCamera
+from time import sleep
+import datetime
+
+camera = PiCamera()
+
+camera.start_preview(alpha=200)
+sleep(10)
+camera.stop_preview()
+
+
+def take_photo():
+    # Get the current date and time and make a unique filename
+
+    # camera.image_effect = 'saturation'
+    camera.start_preview()
+    now = datetime.datetime
+    print(now)
+    filename = "{0:%Y}-{0:%m}-{0:%d}".format(now)
+    # filename = '/home/pi/Desktop/image.jpg'
+    camera.capture(filename)
+    camera.stop_preview()
+
+# Main loop
+
+
+while True:
+    take_photo()
+    sleep(10)
